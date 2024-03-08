56bc767530a0c4dcb298b0d89156339ccccbb097
sooahnlee03 <sl120@wellesley.edu>
2024-03-08 16:15:37 -0500

    //
    combines tiktok data

---
+++ TikTokCombine.py
@@ -0,0 +1,35 @@
+import json
+import pandas as pd
+
+#Read in TikTok json Files
+d1 = json.load(open("/Users/sooahnlee/Desktop/CS/cs315/proj2_data/user_data.json")) #Vid Browse empty
+d2 = json.load(open("/Users/sooahnlee/Desktop/CS/cs315/proj2_data/user_data1.json")) #Vid Browse empty
+d3 = json.load(open("/Users/sooahnlee/Desktop/CS/cs315/proj2_data/user_data2.json"))
+
+#Extract TikTok Video Links from browseList
+browseList = d3['Activity']['Video Browsing History']['VideoList']
+# print(len(browseList)) #27936
+
+vidList = [] #list of tiktok browse history video links
+for dict in browseList:
+    vidList.append(dict['Link'])
+# print(len(vidList)) #27936
+
+#Extract List of Accounts a User is Following
+followingList = d3['Activity']['Following List']['Following']
+# print(len(followingList)) #56
+
+follows = [] #list of username the user follows
+for acct in followingList:
+    follows.append(acct['UserName'])
+# print(len(follows)) #56
+
+#Check if there are Overlaps on Followed Accts vs News Accts
+newsAcctList = pd.read_csv("/Users/sooahnlee/Desktop/CS/cs315/proj2_data/List_of_News_Accounts.csv")
+newsAcct = newsAcctList['Username'].tolist()
+
+overlapAccts = []
+for fw in follows:
+    if fw in newsAcct:
+        overlapAccts.append(fw)
+print(overlapAccts) #There are no overlaps
