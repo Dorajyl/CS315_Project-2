59b85b648b5298e70b8953783ecfe04eeae9b19f
Fernanda Gonzalez <fg100@mit.edu>
2024-04-01 17:45:32 -0400

    data_user corrected

--- data/raw_data/combine_data.py
+++ data/raw_data/combine_data.py
@@ -1,10 +1,17 @@
 import pandas as pd
 
+# Adding original data as user_0001
 original = pd.read_csv("data/raw_data/original_data.csv")
 original["data_user"] = "user_0001"
 
 section_02 = pd.read_csv("data/raw_data/section_02.csv")
 
+# Joining together both datasets and deleting any possible duplicates
 data = pd.concat([original, section_02], ignore_index=True)
+data = data.drop_duplicates()
 
+# Checking that we have 7 users
+print(data['data_user'].drop_duplicates().values)
+
+# Saving as csv
 data.to_csv("data/metadata.csv", index=False)
--- data/raw_data/section_02/section02_combine.py
+++ data/raw_data/section_02/section02_combine.py
@@ -4,7 +4,7 @@ import os
 # Initialize an empty dataframe
 data = pd.DataFrame()
 
-file_num = 2
+file_num = 3
 
 def read_csv_files(folder):
     global data  # Use global keyword to modify the global data DataFrame
@@ -14,8 +14,13 @@ def read_csv_files(folder):
         if file.endswith(".csv"):
             # Read the CSV file into a temporary dataframe
             temp_df = pd.read_csv(f"{folder}/{file}")
-            temp_df["data_user"] = f"user_000{file_num}"
-            file_num += 1
+            
+            # Adding data_user column to seperate by user easily
+            if folder == "data/raw_data/section_02/chunks":
+                temp_df["data_user"] = "user_0002"
+            else:
+                temp_df["data_user"] = f"user_000{file_num}"
+                file_num += 1
             
             # Append the temporary dataframe to the combined dataframe
             data = pd.concat([data, temp_df], ignore_index=True)
@@ -28,4 +33,5 @@ read_csv_files(chunks)
 section_02 = "data/raw_data/section_02"
 read_csv_files(section_02)
 
+# Saving as csv
 data.to_csv("data/raw_data/section_02.csv", index=False)
